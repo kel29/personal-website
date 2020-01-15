b964434b3d99875ff636d27f0dbbc81cc434a4ab
Krista Loven <krista.loven.87@gmail.com>
2020-01-15 14:09:15 -0800

    Add contact information and icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import email from '../icons/email.png'
+import github from '../icons/github.png'
+import linkedin from '../icons/LI.png'
+import resumeIcon from '../icons/resume.png'
+import resume from '../images/KristaLovenResume.pdf'
 
 const Contact = () => {
   return (
-    <div>
-      Contact Me!
+    <div className='contact'>
+      <a href='https://www.linkedin.com/in/krista-loven/' target='blank'>
+        <img src={linkedin} alt='LinkedIn Logo' className='ui mini spaced image' />
+      </a>
+      <a href='https://github.com/kel29' target='blank'>
+        <img src={github} alt='Github Logo' className='ui mini spaced image' />
+      </a>
+      <a href='mailto: krista.loven.87@gmail.com'>
+        <img src={email} alt='email' className='ui mini spaced image' />
+      </a>
+      <a href={resume} target='blank'>
+        <img src={resumeIcon} alt='resume' className='ui mini spaced image' />
+      </a>
     </div>
   )
 }
