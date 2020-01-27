e1bacb3bccd235a541e88b24b42a66d115385c77
Krista Loven <krista.loven.87@gmail.com>
2020-01-27 12:20:41 -0800

    Add new blog post

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,10 +1,17 @@
 export const blogs = [
   {
     id: 1,
-    title: 'DIY Queue: How to write your own Queue class',
+    title: 'DIY Queue: How to Write Your Own Queue Class',
     link: 'https://medium.com/@krista.loven.87/diy-queue-how-to-write-your-own-queue-class-73b94acbec98',
     description: 'Let’s step back to a simpler time, when a line was a line, and if I was the first person in line, then gosh darnit, I was the first person served. That, my friend, is a queue.',
     thumbnail: 'https://miro.medium.com/fit/c/560/168/0*dyluto9A3Hkkj5dR'
+  },
+  {
+    id: 3,
+    title: 'Vectors: How An Array Doesn’t Run Out of Space',
+    link: 'https://medium.com/@krista.loven.87/vectors-how-an-array-doesnt-run-out-of-space-89692c5f98bb',
+    description: `When you make an array in JavaScript, you don't have to specify how much space in memory you need. Why?`,
+    thumbnail: 'https://miro.medium.com/fit/c/1120/336/1*BDdTKS0rwFofe7pL-sod7g.png'
   },
   {
     id: 2,
